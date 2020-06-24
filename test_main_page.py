ac5379ad5c7e0e95c89932daddb1204fa9e1e1f8
Alioshca-Z <Alioshca-Z@yandex.ru>
2020-06-24 14:55:34 +0300

    Added base page and mane page objects

--- test_main_page.py
+++ test_main_page.py
@@ -15,8 +15,16 @@ from selenium.webdriver.support import expected_conditions as EC
 #         # assert здесь для красоты, он никак не влияет на результат прохождения теста
 #         assert button
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     browser.get(link)
+#     login_link = browser.find_element_by_css_selector("#login_link")
+#     login_link.click()
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser): 
+    browser.get(link) 
+    go_to_login_page(browser) 
